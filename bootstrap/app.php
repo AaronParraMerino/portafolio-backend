41002bef8ef42070cdf6108b5f431364982de531
AaronGPM <aaronguillermo96@gmail.com>
2026-04-17 23:17:56 -0400

    fix: app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',   // ← confirma que esta línea existe
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: '*');
+        $middleware->statefulApi();
+
+        // Añade esto para evitar el error 419 en las rutas de la API
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
