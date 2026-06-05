64a216d5c44e551fda46ecb3aa8d1bd4fc2f5a04
AaronGPM <aaronguillermo96@gmail.com>
2026-06-04 21:58:03 -0400

    feat: agregando estadisticas en home

--- routes/api/home.php
+++ routes/api/home.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Api\HomePortfolioController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('home')->group(function () {
+    Route::get('/stats', [HomePortfolioController::class, 'stats']);
+    Route::get('/desarrolladores', [HomePortfolioController::class, 'developers']);
     Route::get('/portafolios-destacados', [HomePortfolioController::class, 'featured']);
     Route::get('/portafolios/{userId}', [HomePortfolioController::class, 'show']);
 });
